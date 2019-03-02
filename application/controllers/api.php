f736fb27013893c26f1b186a3e4108d8076f875b
José Gregorio Calderón <jose_gregorio2791@hotmail.com>
2019-03-02 11:59:05 -0700

    corrections

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -8,53 +8,55 @@ require(APPPATH . '/libraries/Format.php');
 
 class Api extends REST_Controller
 {
-    public function __construct() 
-    {
-		parent::__construct();
-        $this->load->model('car_model');
-        $this->load->model('validator_model');
-    }  
+  public function __construct() 
+  {
+			parent::__construct();
+      $this->load->model('car_model');
+      $this->load->model('validator_model');
+  }  
 
 	public function car_get()
 	{
-		$this->response($this->car_model->read()); 
-  	}
+		  $this->response($this->car_model->read()); 
+	}
 
-  	public function car_post()
-  	{
+	public function car_post()
+	{
   		$car = array(
-			'imei' => $this->input->get('imei'),
-			'status' => $this->input->get('status')
-		);
+					'imei' => $this->input->get('imei'),
+					'status' => $this->input->get('status')
+			);
 
-  		$r = array();
-  		if($this->validator_model->validate_car($car)){
-  			$r['message'] = $this->car_model->insert($car);
-  		}else{
-  			$r['message'] = 'You need to send all parameters to insert new car.';
-  		}
-  		
-		$this->response($r); 
-  	}
+			$r = array();
+
+			if($this->validator_model->validate_car($car)){
+					$r['message'] = $this->car_model->insert($car);
+			}else{
+					$r['message'] = 'You need to send all parameters to insert new car.';
+			}
+
+			$this->response($r); 
+	}
 
 	public function car_put()
-    {
-		$id = $this->uri->segment(3);
+  {
+			$id = $this->uri->segment(3);
 
-		$data = array(
-	  		'imei' => $this->input->get('imei'),
-	  		'status' => $this->input->get('status')
-		);
+			$data = array(
+		  		'imei' => $this->input->get('imei'),
+		  		'status' => $this->input->get('status')
+			);
 
-		$r = $this->car_model->update($id,$data);
-        $this->response($r); 
+			$r = $this->car_model->update($id,$data);
+      $this->response($r); 
 	}
 
 	public function car_delete()
 	{
-		$id = $this->uri->segment(3);
-		$r = $this->car_model->delete($id);
-		$this->response($r); 
+			$id = $this->uri->segment(3);
+			$r = $this->car_model->delete($id);
+			$this->response($r); 
 	}
 }
+
 ?>
--- application/models/Validator_model.php
+++ application/models/Validator_model.php
@@ -3,29 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Validator_model extends CI_Model
 {
-  	
-  	public function validate_car($car)
-  	{
-  		/* 
-  		required car params:
+	public function validate_car($car)
+	{
+			/* 
+			required car params:
 			- imei
 			- status
-  		*/
-  		$valid = true;
+			*/
+		  $valid = true;
 
-  		if($this->validate_is_null($car['imei']))
-  			$valid = false;
-  		if($this->validate_is_null($car['status']))
-  			$valid = false;
+			if($this->validate_is_null($car['imei']))
+					$valid = false;
+			if($this->validate_is_null($car['status']))
+					$valid = false;
 
-  		return $valid;
-  	}
+			return $valid;
+	}
 
-  	public function validate_is_null($param)
-  	{
-  		return ($param === NULL) ? true : false;
-  	}
-
+	public function validate_is_null($param)
+	{
+			return ($param === NULL) ? true : false;
+	}
 }
 
 ?>
